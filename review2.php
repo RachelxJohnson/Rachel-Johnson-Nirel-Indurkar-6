8846eceed7a40835c7c60c38050ce20312b27877
nirelindurkar <95116958+nirelindurkar@users.noreply.github.com>
2021-12-06 15:27:53 -0800

    Reviews
    Added html page for input and output for any user's reviews. I also began adding php for the contact form

---
+++ review2.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+You rated our recipe as <?php echo $_POST["stars"]; ?><br>
+Your review is: <?php echo $_GET["review"]; ?>
+
+</body>
+</html>
