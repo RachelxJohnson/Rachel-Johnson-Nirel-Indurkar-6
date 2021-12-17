5e3236022ecf1554fbce2700b3a71d6d9d932cab
nirelindurkar <95116958+nirelindurkar@users.noreply.github.com>
2021-12-17 10:57:11 -0800

    Add files via upload

---
+++ revMil.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action = "myPhp.php"
+    method = "Post">
+    Name:<input type = "text)"
+    name="name">
+    Recipe:<input type = "text)"
+    name="recipe">
+    stars:<input type = "number"
+    name="stars">
+    <input type = "submit">
+</body>
+</html>
